366e685113f23fc9ac6bba4efd4dd894bac3cafd
lcandyl <zhangqi920503@163.com>
2016-05-28 21:38:23 +0800

    luanqibazaoyidui

---
+++ exercise.java
@@ -0,0 +1,9 @@
+public class exercise
+{
+	public static void main(String[] args){
+		int a =1;
+		System.out.println(a);
+                byte b = 200;
+                System.out.println(b);
+       }
+}
